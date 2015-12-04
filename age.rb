60cfdf613ad834e9325f3ac09a9abecb9ac87a52
Tatsuro Baba <harakirisoul@gmail.com>
2015-12-04 10:26:05 +0900

    Initial commit

---
+++ age.rb
@@ -0,0 +1,13 @@
+0 <= age or raise
+
+if 0 <= age && age <= 2
+  'baby'
+elsif 3 <= age && age <= 6
+  'little child'
+elsif 7 <= age && age <= 12
+  'child'
+elsif 13 <= age && age <= 18
+  'youth'
+else
+  'adult'
+end
